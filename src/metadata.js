247d5ce0810ff646004b752f0fb9b6ed61b882ba
Henrique Picanço <114828539+henriquepicanco@users.noreply.github.com>
2025-12-06 02:21:56 -0300

    Changing metadata to site.json in src/data

--- src/metadata.js
+++
@@ -1,7 +0,0 @@
-export const site = {
-  language: "en-US",
-  name: "Astro Blog Starter",
-  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deserunt quae adipisci possimus nobis sit dolore quo, repellendus illo rerum animi doloremque dicta architecto voluptas beatae minima pariatur similique deleniti.",
-  robots: "index, follow",
-  siteURL: "http://localhost:4321",
-}
